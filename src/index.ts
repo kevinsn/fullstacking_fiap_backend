48fafa07ffa319bfbb340be0c120d71517790064
Kevin <kevinsadao@hotmail.com>
2023-11-12 17:42:53 -0300

    Adicionado exclusão, alteração e finalização de tarefa

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,21 @@ import * as bodyParser from "body-parser";
 import * as cors from "cors";
 import routes from "./routes";
 
+
+
+
 AppDataSource.initialize().then(async () => {
     const app = express();
-
+    const allowedOrigins = ['http://localhost:3000'];
+    cors({
+        origin: function (origin, callback) {
+            if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+                callback(null, true);
+            } else {
+                callback(new Error('Not allowed by CORS'));
+            }
+        },
+    })
     app.use(cors());
     app.use(bodyParser.json());
     app.use(routes);
