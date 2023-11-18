708c1ba265620943ccf32df8aa4f1b8e9e3db130
Kevin <kevinsadao@hotmail.com>
2023-11-18 12:06:08 -0300

    Ajuste cors

--- src/index.ts
+++ src/index.ts
@@ -7,23 +7,10 @@ import routes from "./routes";
 
 AppDataSource.initialize().then(async () => {
     const app = express();
-    const allowedOrigins = ['http://localhost:3000'];
 
-    const corsOptions = {
-        origin: function (origin, callback) {
-            if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-                callback(null, true);
-            } else {
-                callback(new Error('Not allowed by CORS'));
-            }
-        },
-    };
-
-    app.use(cors(corsOptions));
+    app.use(cors());
     app.use(bodyParser.json());
     app.use(routes);
 
-    app.listen(3333, () => {
-        console.log('Server is running on port 3333');
-    });
+    app.listen(process.env.PORT || 3333);
 });
